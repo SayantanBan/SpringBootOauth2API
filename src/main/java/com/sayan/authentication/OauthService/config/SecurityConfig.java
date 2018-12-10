b3bd2368bb701c9ff5b8b44dd412863cff06e805
Sayantan Banerjee <sayantanbanerjee@outlook.com>
2018-12-11 00:38:59 +0530

    Update SecurityConfig.java

--- src/main/java/com/sayan/authentication/OauthService/config/SecurityConfig.java
+++ src/main/java/com/sayan/authentication/OauthService/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("https://api-eutivityrestaurantcrm365.herokuapp.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token", "Authorization"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
