b3c827416aa706758ac6bf4cccffb1a93b96a1d7
Sayantan Banerjee <sayantanbanerjee@outlook.com>
2018-12-11 00:30:36 +0530

    Update AdditionalWebConfig.java

--- src/main/java/com/sayan/authentication/OauthService/config/AdditionalWebConfig.java
+++ src/main/java/com/sayan/authentication/OauthService/config/AdditionalWebConfig.java
@@ -21,7 +21,7 @@ public class AdditionalWebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("https://api-eutivityrestaurantcrm365.herokuapp.com");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
